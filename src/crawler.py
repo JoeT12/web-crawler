8396ff28b88d93f79666e303050937410bd456fc
JoeT12 <joseph.taylor19@hotmail.com>
2026-04-29 12:45:29 +0100

    Adding Additional Comments For GenAI Crawler Implementation

--- src/crawler.py
+++ src/crawler.py
@@ -1,23 +1,29 @@
 from collections import deque
 
-# ChatGPT (5.2 Thinking model) was used to implement stubbed functions. Such stubs had their
-# function defined in detail in a documentation comment (which hasn't been changed) to guide the AI
-# model.
+# To implement the crawler functionality, we used the lecture slides to write function stubs and
+# detailed descriptions for implementation by a GenAI tool. The GenAI tool used to generate the
+# implementations was ChatGPT (5.2 Thinking model). We note that we have not changed the function
+# documentation comments throughout the development process to ensure full visibility as to the
+# information provided to the GenAI for implementation.
 
-# Guidance provided to the AI for all function implementation was as follows:
-# 1. All code should be testable.
+
+# General guidance provided to the AI for all function implementation was as follows:
+# 1. All code should be testable.
 # 2. All code should handle errors gracefully to prevent crashes.
 # 3. All code should be efficient, and concise as possible.
 # 4. All code should be thread-safe to enable multithreaded execution.
+# 5. All code should use the logger to print helpful log messsages that can be used in any debugging.
+
 
 class Crawler:
-    def __init__(self, seeds, politeness_window=6, crawl_limit=1000):
+    def __init__(self, seeds, logger, politeness_window=6, crawl_limit=1000):
         # The Frontier will be a Python dictionary of host dictionaries.
         # Each host will be of the following form {"queue": deque(), "lastAccessed": datetime}.
         # The queue of each host will list all URL's that need crawling on that host.
         # The last accessed time of each host will store the time that a page was last downloaded/crawled from that host.
         self.frontier = {}
 
+        # Maintain a set of crawled URLs to prevent re-crawling the same URL during an execution.
         self.crawled_urls = set()
 
         # Politeness window enforces a time between concurrent requests to a host.
@@ -27,14 +33,18 @@ class Crawler:
         # Keep track of the number of pages (successfully crawled).
         self.pages_crawled = 0
 
-        # Add all seeds used to the frontier
+        # Used to log into the console for easier debugging.
+        self.logger = logger
+
+        # Add all seeds used to the frontier.
         for seed in seeds:
             self.add_url_to_frontier(seed)
 
     def crawl():
         """ This function begins the crawling process. It continously scans the frontier for hosts that can be crawled, and 
-            makes use of the download_web_page and parse_web_page functions to perform the crawl. It ensures that concurrent requests to the same host are spaced by the politeness_window; and that we don't crawl
-            any more pages than the crawl_limit.
+            makes use of the download_web_page and parse_web_page functions to perform the crawl. It ensures that concurrent 
+            requests to the same host are spaced by the politeness_window; and that we don't crawl any more pages than the crawl_limit.
+            It uses the concurrent.futures library to spawn a thread group to speed up the crawling process.
         """
 
     def download_web_page(self, host):
@@ -71,7 +81,7 @@ class Crawler:
         """ This function attempts to add a host object to the crawler frontier. Before adding the host object to the frontier,
             it checks the robots.txt file of the host to ensure crawling is allowed. If it is not allowed, then the host
             object will not be added to the frontier.
-            
+
             Args:
                 hostname (str): The name of the host to add to the frontier.
             Returns:
