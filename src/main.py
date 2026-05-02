8d015bf83c58d5d9cce4233ab8d59c58863ca632
JoeT12 <joseph.taylor19@hotmail.com>
2026-05-02 12:19:40 +0100

    Implementing Shell

--- src/main.py
+++ src/main.py
@@ -1,15 +1,12 @@
 from crawler import Crawler
+from indexer import Indexer
+from search import Search
+
 import logging
-import argparse
 
 
 def main():
     initial_seeds = ["https://quotes.toscrape.com"]
-
-    # CLI Argument Configuration.
-    parser = argparse.ArgumentParser()
-    parser.add_argument("command", help="Command for the Crawler")
-    args = parser.parse_args()
 
     # Logging Configuration.
     logging.basicConfig(
@@ -19,10 +16,39 @@ def main():
     )
     logger = logging.getLogger(__name__)
 
-    # Build Command.
-    if args.command == "build":
-        crawer = Crawler(seeds=initial_seeds, logger=logger)
-        crawer.crawl()
+    # Module initalisations.
+    crawer = Crawler(seeds=initial_seeds, logger=logger, crawl_limit=10000)
+    indx = Indexer(logger=logger)
+    search = Search(logger=logger, indexer=indx)
+
+    # 'Shell'.
+    while True:
+        # Get the command from the standard input.
+        cmd = input(">> ")
+
+        # Break on exit command.
+        if cmd == "exit":
+            break
+
+        # Start the crawl when build command used.
+        if cmd == "build":
+            crawer.crawl()
+
+        # Load the index when the load command is used.
+        if cmd == "load":
+            indx.load_index()
+
+        # Perform a query search when the find command is used.
+        if cmd.startswith("find "):
+            query = cmd[5:]
+            print(search.search(query))
+
+        #  Perform a single term search when the print command is used.
+        if cmd.startswith("print "):
+            term = cmd[6:]
+            tokens = indx.tokenise_tag_content([term])
+            for token in tokens:
+                print(token, indx.get_inverted_index(token))
 
 
 if __name__ == "__main__":
